8d612911438dbb7c1f0a60b82120cffbb0b38f6d
InolabDesarrollo <134556950+InolabDesarrollo@users.noreply.github.com>
2023-05-25 14:13:12 -0500

    Carga de web forms

--- INOLAB_OC/SubirArchivos.aspx.designer.cs
+++ INOLAB_OC/SubirArchivos.aspx.designer.cs
@@ -7,18 +7,11 @@
 // </generado automáticamente>
 //------------------------------------------------------------------------------
 
-namespace INOLAB_OC {
-    
-    
-    public partial class SubirArchivos {
-        
-        /// <summary>
-        /// Control FileUpload1.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.FileUpload FileUpload1;
+namespace INOLAB_OC
+{
+
+
+    public partial class SubirArchivos
+    {
     }
 }
