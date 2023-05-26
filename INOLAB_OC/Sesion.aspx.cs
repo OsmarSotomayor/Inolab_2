1108c6910bb6d72b8c9d7e5bfc1e5a75ded2ef47
InolabDesarrollo <134556950+InolabDesarrollo@users.noreply.github.com>
2023-05-26 16:31:52 -0500

    Refactorizo la clase Session, uso el metodo getDataRow para validar el inicio de sesion y quito la conexion del metodo Ingresar

--- INOLAB_OC/Sesion.aspx.cs
+++ INOLAB_OC/Sesion.aspx.cs
@@ -9,8 +9,6 @@ namespace INOLAB_OC
     {
         //Conexion a la base de datos (para hacer prebas acceder a BrowserPruebas)
         SqlConnection con = new SqlConnection(@"Data Source=INOLABSERVER03;Initial Catalog=BrowserPruebas;Persist Security Info=True;User ID=ventas;Password=V3ntas_17");
-       
-       
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -18,12 +16,20 @@ namespace INOLAB_OC
             lblip.Text = IP.ToString();
         }
 
-        protected void btnSesion_Click(object sender, EventArgs e)
+      
+        public void Log()
         {
-            //Al presionar el boton de ingresar
-            Ingresar();
-        }
+            SqlCommand cmd1 = new SqlCommand("LogWeb", con);
+            cmd1.CommandType = CommandType.StoredProcedure;
+            cmd1.Parameters.Add("@usuario", SqlDbType.VarChar);
+            cmd1.Parameters.Add("@ip", SqlDbType.VarChar);
 
+            cmd1.Parameters["@usuario"].Value = txtUsuario.Text;
+            cmd1.Parameters["@ip"].Value = lblip.Text;
+
+            con.Open();
+            cmd1.ExecuteNonQuery();
+        }
         public void Ingresar()
         {
             try
@@ -33,39 +39,34 @@ namespace INOLAB_OC
                 Session["fecha2"] = "";
 
                 Log();
-                //Se hace la consulta de los datos del usuario que esta ingresando 
-                SqlCommand comando = new SqlCommand("select  Usuario, Password_,Nombre, Apellidos,idarea,idrol,IdUsuario from Usuarios where usuario='" + txtUsuario.Text + "' and password_='" + txtPass.Text + "'", con);
-                DataSet ds = new DataSet();
-                SqlDataAdapter da = new SqlDataAdapter(comando);
-                da.Fill(ds, "tablausuario");
-
-                DataRow dr;
-                dr = ds.Tables["tablausuario"].Rows[0];
-                if ((txtUsuario.Text == dr["Usuario"].ToString()) || (txtPass.Text == dr["Password_"].ToString()))
+                DataRow dataUser;
+                dataUser = Conexion.getDataRow("select  Usuario, Password_,Nombre, Apellidos,idarea,idrol,IdUsuario from Usuarios where usuario='" + txtUsuario.Text + "' and password_='" + txtPass.Text + "'");
+                
+                if ((txtUsuario.Text == dataUser["Usuario"].ToString()) || (txtPass.Text == dataUser["Password_"].ToString()))
                 {
-                    Response.Write("<script>alert('Ingreso de " + dr["Nombre"].ToString() + " " + dr["Apellidos"].ToString() + "');</script>");
+                    Response.Write("<script>alert('Ingreso de " + dataUser["Nombre"].ToString() + " " + dataUser["Apellidos"].ToString() + "');</script>");
                 }
-                string valor = dr["Nombre"].ToString(), idar = dr["IdArea"].ToString(), idr = dr["IdRol"].ToString();
+                string valor = dataUser["Nombre"].ToString(), idar = dataUser["IdArea"].ToString(), idr = dataUser["IdRol"].ToString();
                 //Asignacion de variables globales que s epueden utilizar en otr spaginas dentro de una misma sesion
                 Session["valor"] = valor;
                 Session["idar"] = idar;
                 Session["idr"] = idr;
 
-                Session["Usuario"] = dr["Usuario"].ToString();
+                Session["Usuario"] = dataUser["Usuario"].ToString();
 
 
                 if (idar == "6")
                 {
                     //Accede a servicios asignados mientras sea idar = 6
-                    Session["idUsuario"] = dr["idUsuario"].ToString();
-                    Session["nameUsuario"] = dr["Nombre"].ToString();
+                    Session["idUsuario"] = dataUser["idUsuario"].ToString();
+                    Session["nameUsuario"] = dataUser["Nombre"].ToString();
                     Response.Redirect("ServiciosAsignados.aspx");
                 }
                 else
                 {
                     //Para el caso de que sea Liz la que ingrese, se le mostraran los calendarios de servicios que hay de todos los ingenieros correspondiendo a su area
-                    Session["idUsuario"] = dr["idUsuario"].ToString();
-                    Session["nameUsuario"] = dr["Nombre"].ToString();
+                    Session["idUsuario"] = dataUser["idUsuario"].ToString();
+                    Session["nameUsuario"] = dataUser["Nombre"].ToString();
                     if (Session["idUsuario"].ToString() == "8")
                     {
                         //Form de seleccion de calendario
@@ -92,18 +93,12 @@ namespace INOLAB_OC
             }
         }
 
-        public void Log()
+        protected void btnSesion_Click(object sender, EventArgs e)
         {
-            SqlCommand cmd1 = new SqlCommand("LogWeb", con);
-            cmd1.CommandType = CommandType.StoredProcedure;
-            cmd1.Parameters.Add("@usuario", SqlDbType.VarChar);
-            cmd1.Parameters.Add("@ip", SqlDbType.VarChar);
-
-            cmd1.Parameters["@usuario"].Value = txtUsuario.Text;
-            cmd1.Parameters["@ip"].Value = lblip.Text;
-
-            con.Open();
-            cmd1.ExecuteNonQuery();
+            //Al presionar el boton de ingresar
+            Ingresar();
         }
+
+
     }
 }
