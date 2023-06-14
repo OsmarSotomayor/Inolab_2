ab86e82dca896f9034db5f3e17e977a028b42e4d
InolabDesarrollo <134556950+InolabDesarrollo@users.noreply.github.com>
2023-06-14 13:03:40 -0600

    + Cambios para darle clean Code a la clase FSR

--- INOLAB_OC/FSR.aspx.cs
+++ INOLAB_OC/FSR.aspx.cs
@@ -8,6 +8,7 @@ using static INOLAB_OC.DescargaFolio;
 using INOLAB_OC.Modelo;
 using System.Data;
 using System.Linq.Expressions;
+using DocumentFormat.OpenXml.Bibliography;
 
 public partial class FSR : Page
 {
@@ -18,7 +19,6 @@ public partial class FSR : Page
 
     protected void Page_Init(object sender, EventArgs e)
     {
-        //test dos
         if (!Page.IsPostBack)
         {
             ReportViewer1.ServerReport.ReportServerCredentials = new MyReportServerCredentials();
@@ -44,6 +44,11 @@ public partial class FSR : Page
 
     protected void Page_Load(object sender, EventArgs e)
     {
+        llenarDatosDeServicioEnElPanel();
+        definirVisibilidadYTextoDeBotonesPrincipales();
+    }
+    public void llenarDatosDeServicioEnElPanel()
+    {
         if (Session["idUsuario"] == null)
         {
             Response.Redirect("./Sesion.aspx");
@@ -51,46 +56,12 @@ public partial class FSR : Page
         else
         {
             //Codigo para que aparezca el titulo, sin error de repeticion del numero del folio
-            titulo.Text = "Información de Servicio con numero de folio N°. " +  Session["folio_p"].ToString();
+            titulo.Text = "Información de Servicio con numero de folio N°. " + Session["folio_p"].ToString();
             Nombre_de_usuario.Text = Session["nameUsuario"].ToString();
         }
         consultaDatosFolioServicio();
-
-        if (Estatus_de_folio_servicio.Text.Equals(FINALIZADO))
-        {
-            Btn_Estatus_Servicio.Text = "Guardar Cambios";
-            Btn_actualizar_fechas.Visible = true;
-            Btn_agregar_acciones.Visible = true;
-        }
-        else
-        {
-            Btn_actualizar_fechas.Visible = false;
-            Btn_agregar_acciones.Visible = false;
-        }
-        if (idservicio.SelectedValue.ToString() =="4" || idservicio.SelectedValue.ToString()== "8" || 
-            idservicio.SelectedValue.ToString() == "9")
-        {
-            //En ciertos tipos de servicio, no se podra hacer el cambio de su estado debido a que no deben de poder cambiarlo libremente los ingenieros
-            idservicio.Enabled = false;
-        }
-        else
-        {
-            idservicio.Enabled = true;
-        }
-        int usuario;
-        usuario =Convert.ToInt32(Session["idUsuario"]);
-
-        if(usuario==71)
-        {
-            btnadjuntar.Visible = true;
-        }
-        else
-        {
-            btnadjuntar.Visible = false;
-        }
-
     }
-
+    
     public void consultaDatosFolioServicio()
     {
            string query = "select * from v_fsr where idingeniero = " + Session["Idusuario"] + " and Folio = " + Session["folio_p"] + "; ";
@@ -120,24 +91,61 @@ public partial class FSR : Page
             cmding.Text = informacionServicio["IdIngeniero"].ToString();
             DropDownList8.Text = informacionServicio["Estatusid"].ToString();
             Estatus_de_folio_servicio.Text= informacionServicio["Estatusid"].ToString();
-        
 
-        //Si ya se inicio el servicio, el boton en ves de decor Iniciar, dira Continuar
-       
-        
+           verificarSiContinuaOIniciaServicio();
+
+    }
+
+    public void verificarSiContinuaOIniciaServicio()
+    {
+
         string inicioServicio = Conexion.getText("select top 1 Inicio_Servicio from FSR where Folio = " + Session["folio_p"].ToString() + " and Inicio_Servicio is not null;");
 
         if (inicioServicio != SIN_SERVICIO_INICIADO)
         {
             Btn_Estatus_Servicio.Text = "Continuar Servicio";
         }
-        else if(inicioServicio.Equals(SIN_SERVICIO_INICIADO) || inicioServicio == null )
+        else if (inicioServicio.Equals(SIN_SERVICIO_INICIADO) || inicioServicio == null)
         {
             Btn_Estatus_Servicio.Text = "Iniciar Servicio";
         }
-        
     }
+    public void definirVisibilidadYTextoDeBotonesPrincipales()
+    {
+        if (Estatus_de_folio_servicio.Text.Equals(FINALIZADO))
+        {
+            Btn_Estatus_Servicio.Text = "Guardar Cambios";
+            Btn_actualizar_fechas.Visible = true;
+            Btn_agregar_acciones.Visible = true;
+        }
+        else
+        {
+            Btn_actualizar_fechas.Visible = false;
+            Btn_agregar_acciones.Visible = false;
+        }
+        if (idservicio.SelectedValue.ToString() == "4" || idservicio.SelectedValue.ToString() == "8" ||
+            idservicio.SelectedValue.ToString() == "9")
+        {
+            //En ciertos tipos de servicio, no se podra hacer el cambio de su estado debido a que no deben de poder cambiarlo libremente los ingenieros
+            idservicio.Enabled = false;
+        }
+        else
+        {
+            idservicio.Enabled = true;
+        }
+        int usuario;
+        usuario = Convert.ToInt32(Session["idUsuario"]);
 
+        if (usuario == 71)
+        {
+            btnadjuntar.Visible = true;
+        }
+        else
+        {
+            btnadjuntar.Visible = false;
+        }
+    }
+    
     protected void Actualizar_Datos_Servicio_Click(object sender, EventArgs e)
     {
         try
@@ -166,8 +174,6 @@ public partial class FSR : Page
                 string responsable = responsableHF.Value;
                 string reportado = reportadoHF.Value;
                 string email = emailHF.Value;
-
-                //se hace la actualizacion de los datos dentro de los campos asignados
                 
                 Conexion.executeQuery(" UPDATE FSR SET Marca='" + marca + "', Modelo='" + modelo + "', NoSerie='" + noserie +
                     "', Equipo='" + descripcion + "', IdEquipo_C='" + id + "',Cliente='" + cliente + "',Telefono='" + telefono +
@@ -218,7 +224,7 @@ public partial class FSR : Page
     }
 
     protected void recreatePdfParaServicioFinalizado(string folio)
-    {//Esta función sirve para crear el Folio de un servicio finalizado.
+    {
 
         ServerReport serverReport = ReportViewer1.ServerReport;
         // Set the report server URL and report path
@@ -253,11 +259,11 @@ public partial class FSR : Page
         //Setup the report viewer object and get the array of bytes
         byte[] bytes = ReportViewer1.ServerReport.Render("PDF", null, out mimeType, out encoding, out extension, out streamIds, out warnings);
 
-        if (Estatus_de_folio_servicio.Text == "3")
+        if (Estatus_de_folio_servicio.Text.Equals(FINALIZADO))
         {
-            //aqui iria el codigo para guardar en //Docs debido a que es cuando se renderiza el pdf 
             string nombre_archivo = Session["folio_p"].ToString();
             string fecha_archivo = DateTime.Now.ToString("dd-MM-yyyy HH_mm");
+
             //El nombre del archivo al ser una actualizacion, llevara la fecha y hora a al que se subio la actualizacion
             string filepath = HttpRuntime.AppDomainAppPath + "Docs\\" + nombre_archivo + " " + fecha_archivo + ".pdf";
 
@@ -272,7 +278,6 @@ public partial class FSR : Page
 
     protected void Iniciar_Folio_De_Servicio_Click(object sender, EventArgs e)
     {
-        //Proceso para iniciar el folio con la fechaWeb que seleccione el ingeniero de servicio
         if (Fecha_inicio_servicio.Text.ToString().Equals(""))
         {
             Response.Write("<script>alert('Favor de seleccionar alguna fecha para la el inicio del folio');</script>");
@@ -281,45 +286,33 @@ public partial class FSR : Page
         {
            
             DateTime fechaYhoraDeInicioDeServicio = generarFechaYHoraDeInicioDeServicio();
-            //Checa que sea la fecha del servicio
+            //Pendiente funcionalidad para verificar si la fecha Inicio de servicio es despues de la fecha de inicio real
             
-            object fechaServicio = Conexion.getObject("select FechaServicio from FSR where Folio=" + Session["folio_p"] + " and Id_Ingeniero =" + Session["idUsuario"] + ";");
+           Conexion.executeQuery(" UPDATE FSR SET idStatus = '2' ,Inicio_Servicio='" +
+             DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") + "', WebFechaIni='" + fechaYhoraDeInicioDeServicio.ToString("yyyy - MM - dd HH: mm:ss.fff")
+             + "' where Folio=" + Session["folio_p"] + " and Id_Ingeniero =" + Session["idUsuario"] + ";");
 
-            DateTime fecha_sol = (DateTime)fechaServicio;           
-            DateTime DateObject2 = DateTime.Parse(fecha_sol.ToString("yyyy-MM-dd"));
-
-            //Validacion de si fecha web es despues de fecha servicio
-            //int result = DateTime.Compare(DateObject2, DateObject);
-            //if (result >0)
-            //{
-            //    Response.Write("<script>alert('La fecha de inicio de Folio no puede ser anterior a la fecha de solicitud de servicio');</script>");
-            //}
-            //else
-            //{
-                //Fecha se solicitud anterior o igual a la fecha de inicio
-
-                //update a base de datos y redireccionamient
-                Conexion.executeQuery(" UPDATE FSR SET idStatus = '2' ,Inicio_Servicio='" +
-                    DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") + "', WebFechaIni='" + fechaYhoraDeInicioDeServicio.ToString("yyyy - MM - dd HH: mm:ss.fff")
-                    + "' where Folio=" + Session["folio_p"] + " and Id_Ingeniero =" + Session["idUsuario"] + ";");
-
-            try
-            {
-                
-                string consulta = "Select ClgID FROM SCL5 where U_FSR = " + Session["folio_p"];
-                int idFolioActividadSap = ConexionInolab.getScalar(consulta);
-                ConexionInolab.executeQuery(" UPDATE OCLG SET tel = '" + Session["folio_p"] + " En Proceso' where ClgCode=" + idFolioActividadSap.ToString() + ";");
-               
-            }
-            catch (Exception es)
-            {
-                Console.Write(es.ToString());
-            }
-            Response.Redirect("./DetalleFSR.aspx");
-            
+            actualizarFolioActividadSap();
         }
     }
 
+    public void actualizarFolioActividadSap()
+    {
+        try
+        {
+
+            string consulta = "Select ClgID FROM SCL5 where U_FSR = " + Session["folio_p"];
+            int idFolioActividadSap = ConexionInolab.getScalar(consulta);
+            ConexionInolab.executeQuery(" UPDATE OCLG SET tel = '" + Session["folio_p"] + " En Proceso' where ClgCode=" + idFolioActividadSap.ToString() + ";");
+
+        }
+        catch (Exception es)
+        {
+            Console.Write(es.ToString());
+        }
+        Response.Redirect("./DetalleFSR.aspx");
+    }
+    
     public DateTime generarFechaYHoraDeInicioDeServicio()
     {
         string fechaInicioServicio = Convert.ToDateTime(Fecha_inicio_servicio.Text.ToString()).ToString("yyyy-MM-dd");
@@ -333,8 +326,8 @@ public partial class FSR : Page
 
     protected void Actualizar_fechaYhora_Servicio_Click(object sender, EventArgs e)
     {
-        DateTime fechaYhoraInicioDeFolio = verificarFechaYHoraDeInicioDeFolio();
-        DateTime fechaYhoraCierreFolio = verificarFechaYHoraDeCierreDeFolio();
+        DateTime fechaYhoraInicioDeFolio = traerFechaYHoraDeInicioDeFolio();
+        DateTime fechaYhoraCierreFolio = traerFechaYHoraDeCierreDeFolio();
 
         datepicker2.Text = fechaYhoraInicioDeFolio.ToString("yyyy-MM-dd");
         datepicker3.Text = fechaYhoraCierreFolio.ToString("yyyy-MM-dd");
@@ -373,9 +366,8 @@ public partial class FSR : Page
     }
 
 
-    private DateTime verificarFechaYHoraDeInicioDeFolio()
+    private DateTime traerFechaYHoraDeInicioDeFolio()
     {
-        //Verifica si hay fecha de inicio de folio web o si hay fecha de inicio
         try
         {
             
@@ -396,7 +388,7 @@ public partial class FSR : Page
         }
     }
 
-    private DateTime verificarFechaYHoraDeCierreDeFolio()
+    private DateTime traerFechaYHoraDeCierreDeFolio()
     {
         
         try
