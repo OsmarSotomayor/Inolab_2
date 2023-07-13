537d2b58c56d06b201480f479b870e1425c378e3
InolabDesarrollo <134556950+InolabDesarrollo@users.noreply.github.com>
2023-07-13 12:12:39 -0600

    Agrego controlador y repositorio para tabla OSCL de la base de datos Inolab (SAP)

---
+++ INOLAB_OC/Controlador/Ingenieros/C_OSCL.cs
@@ -0,0 +1,29 @@
+﻿using INOLAB_OC.Modelo;
+using INOLAB_OC.Modelo.Inolab;
+using INOLAB_OC.Modelo.Inolab.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace INOLAB_OC.Controlador.Ingenieros
+{
+    public class C_OSCL
+    {
+        private readonly IOSCL_Repository repositorio;
+
+        public C_OSCL(IOSCL_Repository repositorio) {
+            this.repositorio = repositorio;
+        }
+
+        public void verificarSiSeCierraLaLLamada(string numeroDeValoresEnEstatus, bool nulo, int callId)
+        {
+            if (numeroDeValoresEnEstatus == "1" && nulo == false)
+            {
+                repositorio.cambiarValorDeEstatusFolioAFinalizado(callId);
+
+            }
+        }
+
+    }
+}
---
+++ INOLAB_OC/Modelo/Inolab/OSCL_Repository.cs
@@ -0,0 +1,16 @@
+﻿using INOLAB_OC.Modelo.Inolab.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace INOLAB_OC.Modelo.Inolab
+{
+    public class OSCL_Repository : IOSCL_Repository
+    {
+        public void cambiarValorDeEstatusFolioAFinalizado(int callId)
+        {
+            ConexionInolab.executeQuery("Update OSCL set status = -1 where callID= " +callId);
+        }
+    }
+}
